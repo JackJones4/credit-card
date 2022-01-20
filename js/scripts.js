8bc398e3d53f17635152c8f4e4f985b37ae4af02
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-20 10:47:47 -0800

    complete test3 for luhnAlgorithm function
    Co-authored-by: Jack Jones <jacktjones4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,22 @@
 function luhnAlgorithm(number) {
   const cleanNumber = number.replace(/\s/g, '')
   const array = cleanNumber.split("");
-  console.log(array);
   if (array.length === 16) {
     console.log("This card number is valid.");
   } else {
     console.log("This card number is not valid.");
   }
+
+  const numbers = array.map(Number);
+
+  for (let i = 1; i < numbers.length; i=i+2) {
+    numbers[i] = numbers[i] * 2;
+    if (numbers[i] >= 10) {
+      let digits = numbers[i].toString().split('');
+      let realDigits = digits.map(Number);
+      numbers[i] = realDigits[0]+realDigits[1];
+    }
+  };
+
+  console.log(numbers);
 }
